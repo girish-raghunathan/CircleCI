71c8f2eb0211c2a21d1214fc618394d1088e38dc
Girish Raghunathan <girish_raghu@hotmail.com>
2020-08-10 19:21:05 +0530

    Adding v0.1 calculator.

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Simple calculator program.
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
